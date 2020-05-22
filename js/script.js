4e2f7a2d8ad1dfe4296ac483491a6e6f6e6ee197
Alan Telles <alantelles86@gmail.com>
2020-05-22 18:36:57 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/*
+var jsonCall = document.getElementById('json-res-call');
+var jsonCode = document.getElementById('json-res-area');
+jsonCall.addEventListener('click', function () {
+    jsonCode.style.setProperty('display', 'block');
+})
+*/
+var shown = ""
+var suffix = "-code"
+var codeTogglers = document.querySelectorAll(".code-toggler")
+for (let i = 0; i < codeTogglers.length; i++) {
+    let elem = document.getElementById(codeTogglers[i].id)
+    elem.addEventListener("click", function() {
+        let code_id = codeTogglers[i].id + suffix
+        let code = document.getElementById(code_id)
+        let isHidden = getComputedStyle(code).display == "none"
+        if (isHidden) {
+            if (shown != "") {
+                let codeShown = document.getElementById(shown)
+                codeShown.style.setProperty('display', 'none')
+            }
+            code.style.setProperty('display', 'block')
+            shown = code_id
+        }
+        else
+            code.style.setProperty('display', 'none')
+    })
+}
